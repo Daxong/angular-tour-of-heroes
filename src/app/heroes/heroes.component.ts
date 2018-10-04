d4a2ba0d000179d4150990747e2a7e572566b44c
aFang <daxong.y@gmail.com>
2018-10-04 17:54:16 +0800

    新建heroes组件

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //组件的选择器（css元素选择器）
+  selector: 'app-heroes',
+  //组件模板文件位置
+  templateUrl: './heroes.component.html',
+  //组件私有CSS文件位置
+  styleUrls: ['./heroes.component.css']
+})
+//始终要export这个组件类，方便在其他地方导入
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+  //生命周期钩子 angular在创建完组件后很快就会调用这个 可以放置初始化逻辑
+  ngOnInit() {
+  }
+
+  hero = 'Windstorm';
+}
