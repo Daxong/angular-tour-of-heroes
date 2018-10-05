b282cbc6b06a3587f2b1b8169164c2890f7fb9ae
aFang <daxong.y@gmail.com>
2018-10-05 13:26:39 +0800

    关键词ngModel，AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // 导入该模块，ngModel命令才可用 此处为全局导入
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+//每个组件都必须声明在一个NgModule中一次
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule //然后把FormsModule添加到@NgModule元数据的imports数组 ngModel命令正式生效了
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   //组件的选择器（css元素选择器）
@@ -16,5 +17,9 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
 }
